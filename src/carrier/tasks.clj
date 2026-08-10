e7c853930a6cdf1995db902fce0d1f74cef236c5
Charles Comstock <dgtized@gmail.com>
2026-08-10 18:51:12 -0500

    propagate improvements to release tasks from shimmers

--- src/carrier/tasks.clj
+++ src/carrier/tasks.clj
@@ -21,31 +21,47 @@
        (subs sha 0 8)
        "</code></span>"))
 
-(defn release-file [build-dir]
-  (let [manifest (edn/read-string (slurp (str build-dir "manifest.edn")))]
-    (fs/file-name (get manifest (str build-dir "release-main.js")))))
+;; TODO: use variables from figwheel config?
+(defn default-opts [{:keys [build-dir manifest release] :as opts}]
+  (assoc opts
+         :build-dir (or build-dir "target/public/cljs-out/")
+         :manifest (or manifest "manifest.edn")
+         :release (or release "release-main.js")))
 
-(defn build-static-site [& {:keys [build-dir from to]}]
-  (let [js-dir (str to "/js")]
+(defn release-file [opts]
+  (let [{:keys [build-dir manifest release]} (default-opts opts)
+        manifest-file (str build-dir manifest)
+        release-build (str build-dir release)
+        manifest-db (edn/read-string (slurp manifest-file))]
+    (fs/file-name (get manifest-db release-build))))
+
+(defn build-static-site [& opts]
+  (let [{:keys [build-dir from to release]} (default-opts opts)
+        js-dir (str to "/js")
+        release-base (fs/file-name (fs/strip-ext release))
+        release-glob (str "/" release-base "*")]
     (fs/delete-tree to)
     (println "[carrier] Creating" to "from" from "with javascript")
     (fs/copy-tree from to)
     (fs/create-dirs js-dir)
-    (doseq [js (fs/glob build-dir "**release-main*")]
+    (doseq [js (fs/glob build-dir (str "**" release-base "*"))]
       (fs/copy js js-dir))
-    (bt/shell "bash" "-c" (str "ls -hs --format=single-column " js-dir "/release-main*"))))
+    (bt/shell "bash" "-c" (str "ls -hs --format=single-column " js-dir release-glob))))
 
-(defn rewrite-index [& {:keys [build-dir from to base-href]}]
-  (let [revision (git-revision)
+(defn rewrite-index [& opts]
+  (let [{:keys [index-file from to base-href] :as opts} (default-opts opts)
+        from-index (str (fs/path from index-file))
+        to-index (str (fs/path to index-file))
+        revision (git-revision)
         timestamp (timestamp-iso8601)
-        contents (slurp from)]
-    (println "[carrier] Rewriting" from "->" to)
+        contents (slurp from-index)]
+    (println "[carrier] Rewriting" from-index "->" to-index)
     (println "  build:" timestamp "rev:" revision)
-    (spit to
+    (spit to-index
           (-> contents
               (str/replace-first #"<base href=\"\">"
                                  (str "<base href=\"" base-href "\">"))
               (str/replace-first #"cljs-out\/dev-main\.js"
-                                 (str "js/" (release-file build-dir)))
+                                 (str "js/" (release-file opts)))
               (str/replace-first "<span id=\"revision\"><code>rev:abcdef12</code></span>"
                                  (revision-span timestamp revision))))))
